bbc59992d5ed6980df1bba8da9dd1c4e9044c0f8
João Pedro Ferreira da Cruz <pedro.dacruz@dcomp.ufs.br>
2025-10-19 14:49:39 -0300

    Colocando menu de pausa

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+public class PauseController : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+    public void Resume()
+    {
+        ResumeGame();
+    }
+}
+
