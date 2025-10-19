cfc9fbdf004dfea19acf56560bfb7d3d1409c87c
Jose Gabriel <jgr280704@gmail.com>
2025-10-18 22:45:39 -0300

    Testar no Windows

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,53 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    public float playerSpeed;
-    private Rigidbody2D playerRigidBody;
-    private bool playerRightSide = false;
-    private float playerDirection;
+    [SerializeField] private float playerMaximumMoveSpeed = 4;
+    [SerializeField] private float jumpForce = 5;
+    private Rigidbody2D _playerRigidBody;
+    private bool _playerRightSide = false;
+    public Vector2 _playerDirection;
+    
+    [SerializeField] private InputActionReference moveReference;
+    [SerializeField] private InputActionReference interactReference;
+    [SerializeField] private InputActionReference jumpReference;
+        
     private void Awake()
     {
-        playerRigidBody = GetComponent<Rigidbody2D>();
+        _playerRigidBody = GetComponent<Rigidbody2D>(); ;
+        interactReference.action.performed += Interact;
+        jumpReference.action.performed += Jump;
     }
+    
+
     void Update()
     {
-        playerDirection = Input.GetAxis("Horizontal");
-        playerRigidBody.linearVelocity = new Vector2(playerDirection * playerSpeed, playerRigidBody.linearVelocity.y);
-
-        if (playerDirection > 0 && !playerRightSide || playerDirection < 0 && playerRightSide) TurnPlayer();
+        _playerDirection = moveReference.action.ReadValue<Vector2>();
     }
+
+    private void FixedUpdate()
+    {
+        _playerRigidBody.linearVelocity = new Vector2(_playerDirection.x * playerMaximumMoveSpeed, _playerRigidBody.linearVelocity.y);
+
+        if (_playerDirection.x > 0 && !_playerRightSide || _playerDirection.x < 0 && _playerRightSide) TurnPlayer();
+    }
+
     private void TurnPlayer()
     {
-        playerRightSide = !playerRightSide;
+        _playerRightSide = !_playerRightSide;
         transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        _playerRigidBody.linearVelocity = new Vector2(_playerDirection.x, _playerRigidBody.linearVelocity.y * jumpForce);
+    }
+
+    private void Interact(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Interact");
     }
 }
