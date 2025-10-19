cd294268d5a710feb62df98288a867f71c8ca5f7
Pablo-oficial <131979767+Pablo-oficial@users.noreply.github.com>
2025-10-19 14:24:06 -0300

    ajustes nas cenas

---
+++ Assets/Scripts/Managers/SceneManager/TrocaCena.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Desk : MonoBehaviour
+{
+    [Header("Proxima Cena")]
+	[SerializeField] private int _nextScene;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(EsperaExecuta(0.8f, () => SceneManager.LoadScene(_nextScene)));
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
+    private IEnumerator EsperaExecuta(float time,System.Action action)
+	{
+		yield return new WaitForSeconds(time);
+		action ?.Invoke();
+	}
+}
